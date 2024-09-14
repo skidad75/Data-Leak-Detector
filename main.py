b27fcdbd75c25f994849fb6668dac8789770d8d5
Ryan S <skidad75@gmail.com>
2024-09-13 18:39:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ This tool is designed to help you identify potential data leaks and security vul
 3. Set the maximum crawl depth.
 4. Click "Run Analysis" and wait for the results.
 
+### Pages:
+
+""")
+
+# Update these lines
+st.page_link("pages/web_scraper_app.py", label="Web Scraper 🌐")
+st.page_link("pages/Wall_of_Sheep.py", label="Wall of Sheep 🐑")
 
 st.markdown("""
 ### Important Note:
@@ -36,9 +43,6 @@ This tool is for educational and ethical testing purposes only. Always ensure yo
 The developers of this tool are not responsible for any misuse or damage caused by this program. Use at your own risk and always adhere to ethical guidelines and legal requirements when conducting security assessments.
 """)
 
-st.sidebar.markdown("### Quick Links")
-st.sidebar.page_link("pages/web_scraper_app.py", label="Start Scanning 🚀")
-st.sidebar.page_link("pages/Wall_of_Sheep.py", label="View Wall of Sheep 🐑")
 
 st.sidebar.markdown("---")
 st.sidebar.info("Developed with ❤️ by Your Team")
